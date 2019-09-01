b88eb137dfa902e68f22e9f28ba1049931b7ea2f
Chheang Sotheng <chheangsotheng@yahoo.com>
2019-09-01 12:48:21 +0700

    Seeding part of Product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(0.1,1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2,30),
     ];
 });
